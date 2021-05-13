12aba7501938154c844b0c4a79367aa99f449440
NamKim-il <namil208@korea.ac.kr>
2021-05-13 22:20:39 +0900

    main page components

--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -1,25 +1,136 @@
 import React from 'react';
-import logo from '../logo.svg';
-
+import styled from 'styled-components'
+import Percentage from './Percentage'
+import{ReactComponent as  StopImage} from '../stop.svg' 
 function Main() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>
+      <header>
+        <StopImage id="head-image"/>
+        <div >
+          <BigText>멈춰!</BigText>
+          <div id="head-text">혐오 표현 필터링</div>
+        </div>
       </header>
-    </div>
+      <main>
+        <div className="info">
+          <div className="info-text">
+            <BigText>
+              혐오표현이 포함된 댓글을 블라인드 처리해주는 <br/>
+              크롬 확장앱 <span>멈춰!</span> 서비스의 데모 페이지입니다
+            </BigText>
+            <SmallText>
+              유튜브 댓글 중 혐오성 발언을 블라인드 해드립니다<br/>
+              넘쳐나는 혐오 표현에서 벗어나 마음의 평화를 찾아보세요
+            </SmallText>
+          </div>
+
+
+        </div>
+        
+        <div>
+        <Percentage/>
+        </div>
+
+        <footer>
+        <StopImage height="auto" />
+        <div className="foot-text">
+          <div>
+            혐오 표현이 없는  <br></br>클린한 인터넷 문화를 응원합니다
+          </div>
+          <div className="copy">
+            Copyright © 2021 SW maestro - Minchodan <br></br>
+            개발기 및 코드 구경가기
+          </div >
+
+        </div>
+        
+        </footer>
+      </main>
+     
+      
+    </Container>
+
   );
 }
 
+const Container = styled.div`
+  header{
+    //position : fixed;
+    display : flex;
+    align-items : center;
+    height: 80px;
+    max-height: 70px;
+    margin : 30px;
+  }
+  footer{
+    position : fixed;
+    display : flex;
+    align-items : center;
+    background-color : white;
+    border-top : 3px solid #111111;
+    bottom : 0;
+    width : 100%;
+    height : 5%;
+    min-height : 20px;
+    overflow : hidden;
+    font-size: calc(3px + 1vmin);
+  }
+  .info{
+    display : flex;
+    height : 300px;
+    margin: 80px;
+  }
+  .info-text{
+    display : flex;
+    flex-direction : column;
+  }
+  .foot-text{
+  display : flex;
+  width : 100%;
+  justify-content : space-between;  
+  }
+  .copy{
+  text-align:end;
+  }
+  #head-image{
+    height : 90%;
+    margin : 10px;
+  }
+  #head-text{
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 25px;
+    display: flex;
+    align-items: center;
+  }
+  #co{
+    color : rgb(127,192,128);
+  }
+`
+const BigText=styled.div`
+  font-size : 32px;
+  font-family: Mulish;
+  font-style: normal;
+  line-height: 45px;
+  font-weight: bold;
+  span{
+    color : rgb(127,192,128);
+  }
+
+`
+const SmallText=styled.div`
+  display : flex;
+  align-items: center;
+  height : 80px;
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 19px;
+  line-height: 29px;
+  color: #1F2041;
+  opacity : 0.5;
+`
 export default Main;
---
+++ front/src/pages/Percentage.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Second = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`
+
+const SearchBox = styled.input`
+    width: 791px;
+    height: 56px;
+    background: #FCFCFC;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    margin-right: 15px;
+    &::-webkit-input-placeholder {
+      text-align: center;
+    }
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 56px;
+    /* or 224% */
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: rgba(31, 32, 65, 0.6);
+    border: none;
+`
+
+const SubmitBtn = styled.a`
+    width: 107px;
+    height: 56px;
+    background: linear-gradient(180deg, #9FE7B3 0%, #97D9B9 100%);
+    border-radius: 8px;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 17px;
+    line-height: 21px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    color: #FFFFFF;
+`
+
+const SearchWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+
+const Result = styled.div`
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  line-height: 56px;
+  /* identical to box height, or 224% */
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: rgba(31, 32, 65, 0.6);
+`
+
+const ShowPercentage = styled.span`
+    color: #9CC797;
+`
+
+function Percentage() {
+  const [sentence, setSentence] = useState<string>('')
+  const [percentage, setPercentage] = useState<number>(0)
+  const [visibilty, setVisiblity] = useState<boolean>(false)
+
+  axios.defaults.xsrfCookieName = 'csrftoken';
+  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+
+  async function getPercentage(sentence: string) : Promise<number> { 
+    return await axios.post(`/percentage`,{sentence})
+    .then(res => res.data);
+  }
+
+  const enterSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter') {
+        onSubmit();
+    }
+  }
+
+  async function onSubmit() {
+    if(!visibilty) setVisiblity(true);
+    setPercentage(await getPercentage(sentence));
+  }
+
+  return (
+    <Second>
+      <SearchWrapper>
+        <SearchBox
+            placeholder="나쁜 댓글을 입력하고 혐오 퍼센티지를 확인해보세요"
+            value={sentence}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSentence(e.target.value)}
+            onKeyPress={(e: React.KeyboardEvent<HTMLInputElement>) => enterSearch(e)}
+        />
+        <SubmitBtn onClick={onSubmit}> 확인해보기</SubmitBtn>
+      </SearchWrapper>
+      { visibilty && 
+        <Result>입력하신 댓글의 혐오 지수는&nbsp;<ShowPercentage> {percentage}% </ShowPercentage>&nbsp;입니다</Result>
+      }
+    </Second>
+  );
+}
+
+export default Percentage;
