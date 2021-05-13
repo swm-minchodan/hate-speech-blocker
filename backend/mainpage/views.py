0095b0e2fdb3b622e6ceb0b45a37c12352ff6caa
Sam So <robotronic0694@gmail.com>
2021-05-13 19:59:54 +0900

    add comments response function

--- backend/mainpage/views.py
+++ backend/mainpage/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.utils.decorators import method_decorator
 import json
+from googleapiclient.discovery import build
 
 @ensure_csrf_cookie
 @method_decorator(csrf_exempt, name='dispatch')
@@ -16,5 +17,26 @@ def index(request):
         return JsonResponse('Hello, world!', safe=False)
     elif request.method == 'POST':
         data = json.loads(request.body) # create(form을 이용하여 submit한 형태) 
-        sentence = data['sentence']
-        return JsonResponse(len(sentence), safe=False)
+        
+        video_id = data['sentence']
+        api_key = 'AIzaSyCxWNU8vI-FUOaK0RQYhcHF5yuafhbSsxg'
+        
+        comments = list()
+        api_obj = build('youtube', 'v3', developerKey=api_key)
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=30, order='relevance').execute()
+        
+        while response:
+            for item in response['items']:
+                yt_comment = item['snippet']['topLevelComment']['snippet']
+                comment = {}
+
+                comment["comment"] = yt_comment['textDisplay']
+                comment["author"] = yt_comment['authorDisplayName']
+                comment["author_image_url"] = yt_comment['authorProfileImageUrl']
+                comment["date"] = yt_comment['publishedAt']
+                comment["num_likes"] = yt_comment['likeCount']
+
+                comments.append(comment)
+            break
+
+        return JsonResponse({"comments": json.dumps(comments, ensure_ascii=False)}, safe=False)
